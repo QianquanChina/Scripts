b74650bc57b607e8699609e409e68f1699e856ae
jc <370047797@qq.com>
2022-04-09 11:22:57 +0800

    picom config

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash 
+#xrandr --output HDMI-1-0 --mode 2560x1440 --rate 144 --output eDP-1 --off
+xrandr --output HDMI-1-0 --mode 1920x1080 --rate 144 --output eDP-1 --off
 /bin/bash ~/Scripts/dwm/dwm_refresh.sh &
-feh --bg-scale --no-fehbg ~/Downloads/BackGround/1638778106362.jpg 
+feh --bg-scale 1-no-fehbg ~/Downloads/BackGround/1.png
 fcitx5 &
-xcompmgr &
+sleep 1 && picom --config ~/Scripts/dwm/config/compton.conf &
+# xcompmgr &
 ~/Scripts/dwm/autostart_wait.sh
