cfea0130fdf734d3a3438778c342dc995405511b
jc <370047797@qq.com>
2022-05-01 21:20:42 +0800

    trayer script

---
+++ dwm/pallets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+result=$(ps ax|grep -v grep|grep trayer)
+echo $result
+if [ "$result" == "" ]; then
+    eval "trayer --edge top --width 5 --height 23 --align right --distance 10 --margin 14 --transparent true --tint 10 &"
+else
+    eval "killall trayer"
+fi
