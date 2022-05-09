cd53292f28443e2009a7bed87a6b9732df806789
jc <370047797@qq.com>
2022-05-09 22:15:22 +0800

    trayer config

--- dwm/pallets.sh
+++ dwm/pallets.sh
@@ -2,7 +2,7 @@
 result=$(ps ax|grep -v grep|grep trayer)
 echo $result
 if [ "$result" == "" ]; then
-    eval "trayer --edge top --width 5 --height 23 --align right --distance 10 --margin 14 --transparent true --tint 10 &"
+    eval "trayer --edge top --width 5 --height 27 --align right --distance 10 --margin 14 --transparent true --tint 10 &"
 else
     eval "killall trayer"
 fi
